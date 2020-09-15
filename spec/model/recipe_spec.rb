ff40cd7f95ee02c45a50788fc4f5fd947956fde0
Hugo Nugra <hugonugra@MacBook-Pro-de-Hugo-2.local>
2020-09-15 17:11:33 +0200

    update recipe tests

--- spec/model/recipe_spec.rb
+++ spec/model/recipe_spec.rb
@@ -1,8 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Recipe, :type => :model do
+
   it "is valid creating a new object" do
-    expect(Recipe.new()).to be_valid
+    expect(Recipe.new("example", "path/to/picture", ["a","b","c"], "a description", "Hugo")).to be_valid
+  end
+
+  it "is not valid creating a new object without all params" do
+    expect(Recipe.new()).to_not be_valid
   end
 
   it "list all recipes" do 
